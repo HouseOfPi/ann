cbe6a9914922eda0060b8905eac99061b10ecc1d
mosesmathew <moseslmathew@gmail.com>
2026-05-21 13:58:22 +0530

    Set single variable slide as the default page on load

--- js/scripts.js
+++ js/scripts.js
@@ -2200,7 +2200,7 @@
       // Activate default slide on load
       {
         const activeSlides = getActiveSlides();
-        const idx = activeSlides.findIndex(el => el.id === 'slideLinearNVar');
+        const idx = activeSlides.findIndex(el => el.id === 'slideLinearSingleVar');
         currentSlideIdx = idx >= 0 ? idx : 0;
         const el = activeSlides[currentSlideIdx];
         if (el) {
